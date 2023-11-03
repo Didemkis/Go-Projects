d8b204c5f4659786bd9198f7fb5dbd34f6be8a64
Didemkis <didemkis09@gmail.com>
2023-11-03 20:02:42 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main //Dosyayı hangi paketin içine koyucağımızı belirleriz.
+import "fmt" // Aşağıda ki kodu yazıp Ctrl+ S ile otomatik import ediyor.
+
+func main() { // Go kodlarımızın çalışacağı bölüm
+
+	fmt.Print("Hello ")                    //Ekrana yazdırma
+	fmt.Println("Go dilini öğreniyorum. ") //Çıktının alt alta yazdığını görüyoruz .Nedeni ise Println 'de ki ln 'den kaynaklıdır. Line demektir ve bu komut ile alt alta çıktı sağlar.
+	fmt.Println("!")
+
+	var metin string = "Go dilinde değişken tanımlamayı öğreniyorum. " //Değişken tanımlamak için var deyimi kullanılır. Hemen sonrasında değişkenin adı ve türü belirtilir. Açık değişken atama işlemi.
+	fmt.Println(metin)                                                 //Değişkenimizi yazdırıyoruz.
+
+	hesap := 100 // Kapalı değişken atama.
+	fmt.Println(hesap)
+	// Veri tipi öğrenmek istersek
+	fmt.Printf("veri tipi : %T", hesap) //
+	// Printf = formatını yani veri tipini bize söyler ."%T"  yüzde işareti kendinden sonraki ilkini yazdırır T işareti de type'ını yazdırır
+
+	//Örnek
+	var durum bool
+
+	var metin1 string = "didem"
+	var metin2 string = "karamel"
+
+	durum = metin1 == metin2
+	fmt.Println(durum)
+}
