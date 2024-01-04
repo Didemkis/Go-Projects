ec49f4befbe84ba039c79e3b5dd74b42b716d15b
Didem kış <78049125+Didemkis@users.noreply.github.com>
2024-01-04 21:45:13 +0300

    forloop.go

--- loops/forloop.go
+++ loops/forloop.go
@@ -13,3 +13,12 @@ func Ornek1() {
 	}
 	fmt.Println("bitti")
 }
+// İKİNCİ KULLANIM
+
+func Ornek2() {
+	for i := 1; i < 10; i++ { //ilk başlangıç değerini verdim , ikinci kısım şart kısmı ve i'yi arttırma
+		fmt.Println(i)
+	}
+	fmt.Println("Bitti.")
+}
+
